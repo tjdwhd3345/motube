162560bb4e52b8b3311f7f1a2763c21cf3c80c12
mocci <tjdwhd3345@gmail.com>
2023-03-02 00:31:39 +0900

    test: add testcode for header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,53 +1,41 @@
-import React, { PureComponent } from "react";
+import React, { useState } from "react";
 import styles from "./header.module.css";
 
-class Header extends PureComponent {
-  inputRef = React.createRef();
+function Header({ onSearchClick }) {
+  const [keyword, setKeyowrd] = useState("");
 
-  handleSearch = () => {
-    const keyword = this.inputRef.current.value;
-    console.log("handleClick", keyword);
-    this.props.onSearchClick(keyword);
-    this.inputRef.current.blur();
-  };
-
-  handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      this.handleSearch();
-    }
-  };
-
-  handleClick = (e) => {
-    this.handleSearch();
-  };
-
-  handleLogoClick = () => {
+  const handleLogoClick = () => {
     window.location = "/motube";
   };
 
-  render() {
-    console.log("header.jsx render");
-    return (
-      <>
-        <header className={styles.header}>
-          <div className={styles.logoContainer} onClick={this.handleLogoClick}>
-            <img src="images/logo.png" alt="logo" className={styles.logo} />
-            <span className={styles.title}>MoTube</span>
-          </div>
-          <input
-            ref={this.inputRef}
-            type="search"
-            className={styles.searchInput}
-            placeholder="검색"
-            onKeyPress={this.handleKeyPress}
-          />
-          <button className={styles.searchButton} onClick={this.handleClick}>
-            <img src="images/search.png" alt="search" />
-          </button>
-        </header>
-      </>
-    );
-  }
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      onSearchClick(keyword);
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer} onClick={handleLogoClick}>
+        <img src="images/logo.png" alt="logo" className={styles.logo} />
+        <span className={styles.title}>MoTube</span>
+      </div>
+      <input
+        value={keyword}
+        type="search"
+        className={styles.searchInput}
+        placeholder="검색"
+        onChange={(e) => {
+          const { value } = e.target;
+          setKeyowrd(value);
+        }}
+        onKeyUp={handleKeyUp}
+      />
+      <button className={styles.searchButton} onClick={() => onSearchClick(keyword)}>
+        <img src="images/search.png" alt="search" />
+      </button>
+    </header>
+  );
 }
 
 export default Header;
